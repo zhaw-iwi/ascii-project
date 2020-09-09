eea150e44feb7eed055e82bc87fcae7e4c708945
Adrian Moser <mosa@zhaw.ch>
2020-09-09 11:28:16 +0000

    improve php

--- index.php
+++ index.php
@@ -1,1 +1,8 @@
 <?php header( 'Location: /README.md' ); ?>
+
+<?php
+    use Michelf\Markdown;
+    $readme = file_get_contents("/README.md");
+    $html = Markdown::defaultTransform($readme);
+    print $html;
+?>

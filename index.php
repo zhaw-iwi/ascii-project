f982fdc2c28acec33b1ee9631b89fdf434b9dc02
Adrian Moser <mosa@zhaw.ch>
2020-09-09 11:33:27 +0000

    improve script

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
+    require_once 'Michelf/Markdown.inc.php';
     use Michelf\Markdown;
-    $readme = file_get_contents("/README.md");
+    $readme = file_get_contents(__DIR__ . '/README.md');
     $html = Markdown::defaultTransform($readme);
     print $html;
 ?>
